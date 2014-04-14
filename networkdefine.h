0a9a1715c4b16aa1e72d5fac83b8590c2f231ce4
firekillice <firekillice@163.com>
2014-04-14 17:34:11 +0800

    定义两个头文件

---
+++ networkdefine.h
@@ -0,0 +1,127 @@
+
+/******************************************************************* 
+ *  Copyright(c) 2000-2013 linghegu
+ *  All rights reserved. 
+ *   
+ *  文件名称: define normal infomation of network
+ *  简要描述: include the usual network header,and predefine
+ *   
+ *  创建日期: 2014/4/14
+ *  作者:	      wangbin
+ *  说明: 	      
+ ******************************************************************/
+#ifndef __NETWORK_DEFINE_H__
+#define __NETWORK_DEFINE_H__
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/poll.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif		//endof __NETWORK_DEFINE_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
