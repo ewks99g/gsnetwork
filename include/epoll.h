525d97469991c7f488710f050621d21be4a12d8c
firekillice <firekillice@163.com>
2014-04-16 11:01:28 +0800

    add epoll demo

---
+++ include/epoll.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "networkdefine.h"
+#include "err.h"
+#include "ioevent.h"
+#include "thread.h"
+
+class CEpoll 
+{
+	public:
+		CEpoll();
+		~CEpoll();
+	public:
+		bool start();
+		void loop();
+		bool addHandler(THandler handler,CIoEvent* ioevent);
+		bool rmvHandler(THandler handler);
+
+		 //  Main worker thread routine.        
+		 static void workerRoutine (void* arg);
+	private:
+		THandler 		m_nEpollHandler;
+		epoll_event		m_vEventCache[MAX_EPOLL_NETWORK_EVENT_SIZE];
+
+		CThread			m_worker;
+};
---
+++ include/ioevent.h
@@ -0,0 +1,32 @@
+
+/******************************************************************* 
+ *  Copyright(c) 2000-2013 linghegu
+ *  All rights reserved. 
+ *   
+ ******************************************************************/
+#pragma once
+
+#include "networkdefine.h"
+
+class CIoEvent
+{
+	public:
+		CIoEvent() {}
+		~CIoEvent() {}
+	public:
+	
+	/*
+	 * This method is used to handle the input event from event listener,i.e. epoll.
+	 * */
+	virtual void in_event() = 0;
+	
+	/*
+	 *This method is used to handler the output event from event listener,i.e. epoll.
+	 * */
+	virtual	void out_event() = 0;
+
+	/*
+	 *This method is used to handler the timeout event from event listener,i.e. epoll.
+	 * */
+	virtual	void time_event() = 0
+};
